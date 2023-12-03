97118ede12d4cfaff8c04b4daf2e86bdb965e06c
KN <64144072+ndknitor@users.noreply.github.com>
2023-12-03 22:07:52 +0700

    Update and rename sqlserver.sql to Database.sql

--- Database.sql
+++ Database.sql
@@ -12,8 +12,23 @@ create table Product
 )
 select * from Product where [Name] like '%me%'  COLLATE  SQL_Latin1_General_CP1_CI_AI
 
+-- Rename table Sql Server
+BEGIN TRANSACTION;
+	EXEC sp_rename 'OldTableName', 'NewTableName';
+COMMIT;
+
+-- Rename table MySql
+START TRANSACTION;
+  RENAME TABLE old_table_name TO new_table_name;
+COMMIT;
+
+-- Reaname table Postgres
+BEGIN;
+  ALTER TABLE old_table_name RENAME TO new_table_name;
+COMMIT;
 
 
 -- Colation for non-symbol Vietnamese searching in MariaDn
 -- utf8mb4_unicode_520_ci
 --- utf8mb4_unicode_520_nopad_ci
+
