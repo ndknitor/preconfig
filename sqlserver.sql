440b59bed6d313a1b69895fc7167aefd7b5113ab
KN <64144072+ndknitor@users.noreply.github.com>
2023-12-03 21:34:27 +0700

    Update sqlserver.sql

--- sqlserver.sql
+++ sqlserver.sql
@@ -1,24 +1,19 @@
--- Step 1: Create a Login (if necessary)
 CREATE LOGIN YourLoginName
 WITH PASSWORD = 'YourPassword';
-
--- Step 2: Create a User in a specific database
 USE YourDatabaseName;
 CREATE USER YourUserName FOR LOGIN YourLoginName;
-
--- Step 3: Grant Permissions
--- Example: Grant SELECT, INSERT, UPDATE, DELETE permissions on a table
 GRANT SELECT, INSERT, UPDATE, DELETE ON YourTableName TO YourUserName;
 
--- Example: Grant EXECUTE permission on a stored procedure
-GRANT EXECUTE ON YourStoredProcedureName TO YourUserName;
-
-
-
--- Colation for non-symbol Vietnamese searching
+-- Colation for non-symbol Vietnamese searching in Sql Server
 create table Product
 (
 	ProductId int identity primary key,
 	[Name] nvarchar(128) not null default '' COLLATE SQL_Latin1_General_CP1_CI_AI
 )
 select * from Product where [Name] like '%me%'  COLLATE  SQL_Latin1_General_CP1_CI_AI
+
+
+
+-- Colation for non-symbol Vietnamese searching in MariaDn
+-- utf8mb4_unicode_520_ci
+--- utf8mb4_unicode_520_nopad_ci
