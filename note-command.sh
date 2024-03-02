e04fddc3ddc50df788a7d9a9052ac013f5e704af
KN <64144072+ndknitor@users.noreply.github.com>
2024-03-02 11:44:42 +0700

    Update note-command.sh

--- note-command.sh
+++ note-command.sh
@@ -10,6 +10,10 @@ git config --global credential.helper "cache --timeout=999999999"
 git credential-cache exit
 #####################
 
+# Instantly create a reverse porxy
+mitmproxy --listen-port 8000 --mode reverse:https://example.com/path
+#####################
+
 # Block ping
 sudo echo "net.ipv4.icmp_echo_ignore_all = 1" >> /etc/sysctl.conf 
 sudo sysctl -p 
