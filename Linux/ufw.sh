cb1cd1bca11cbaf2356281aea63976296a0dc015
KN <64144072+ndknitor@users.noreply.github.com>
2024-07-10 08:35:39 +0700

    Update ufw.sh

--- Linux/ufw.sh
+++ Linux/ufw.sh
@@ -17,3 +17,6 @@ sudo ufw allow from 192.168.1.0/24 to any port 80
 
 # Deny a subnet on port 80
 sudo ufw deny from 192.168.1.0/24 to any port 80
+
+# Allow ip on port with xargs
+cat allowed-ip.txt | xargs -I {} sudo ufw allow from {} to any port 80
