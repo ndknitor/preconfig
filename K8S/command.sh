accc2ec24a3a241d62c2d0578c9cd544e32737d2
KN <64144072+ndknitor@users.noreply.github.com>
2024-12-18 22:06:49 +0700

    Create command.sh

---
+++ K8S/command.sh
@@ -0,0 +1,18 @@
+#### Login to registtry
+kubectl create secret docker-registry <name> --docker-server=<your-registry-server> --docker-username=<your-name> --docker-password=<your-pword>
+
+### Configure RBAC ArgoCD
+kubectl edit configmap argocd-rbac-cm -n argocd
+########################################
+data:
+  policy.csv: |
+    p, role:full-access, *, *, *, allow
+    g, webhook, role:full-access
+########################################
+
+##### Add user to AgrgoCD
+kubectl edit configmap argocd-cm -n argocd 
+########################################
+data:
+  accounts.webhook: apiKey
+########################################
