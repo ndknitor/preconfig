4e3bfda98ab77764f607ef84c35666457894a967
KN <64144072+ndknitor@users.noreply.github.com>
2024-12-26 15:27:03 +0700

    Update flameshot-ocr.sh

--- Personal/flameshot-ocr.sh
+++ Personal/flameshot-ocr.sh
@@ -1,14 +1,21 @@
 #! /bin/bash
 
+export TESSDATA_PREFIX="$HOME/.local/share/tessdata"
+
 available_langs=$(tesseract --list-langs 2>/dev/null)
 available_langs=$(echo "$available_langs" | tail -n +2)
 
 if [[ ! -z "$available_langs" ]]; then
   language=$(zenity --list --title="Available OCR Languages" \
-                  --text="Choose language:" \
-                  --column="Language" <<< "$available_langs")
+                    --text="Choose language:" \
+                    --column="Language" <<< "$available_langs")
+  if [[ -z "$language" ]]; then
+    echo "Operation canceled by the user."
+    exit 1
+  fi
 else
   echo "No languages found for Tesseract."
+  exit 1
 fi
 
 sleep 0.5
