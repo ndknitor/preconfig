9f2176f44b8eadfad5658c4217ce64db13eb4f15
KN <64144072+ndknitor@users.noreply.github.com>
2024-12-31 06:49:13 +0700

    Update command.sh

--- K8S/command.sh
+++ K8S/command.sh
@@ -1,5 +1,37 @@
-#### Login to registtry
-kubectl create secret docker-registry <name> --docker-server=<your-registry-server> --docker-username=<your-name> --docker-password=<your-pword>
+### Create generic secret
+kubectl create secret generic asp-template-secret \
+  --from-literal=apiKey=your-api-key-value \
+  --from-literal=databaseUrl=your-database-url \
+  --namespace=your-namespace \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+### Create tls secret
+kubectl create secret tls tls-secret-name \
+  --cert="path/to/your-cert.pem" \
+  --key="path/to/your-key.pem" \
+  --namespace=your-namespace \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+
+### Create tls secret from value
+kubectl create secret tls tls-secret-name \
+  --cert=<(echo "-----BEGIN CERTIFICATE-----
+<your-certificate-content>
+-----END CERTIFICATE-----") \
+  --key=<(echo "-----BEGIN PRIVATE KEY-----
+<your-private-key-content>
+-----END PRIVATE KEY-----") \
+--namespace=your-namespace \
+--dry-run=client -o yaml | kubectl apply -f -
+
+
+#### Create registtry credential
+kubectl create secret docker-registry <name> \ 
+--docker-server=<your-registry-server> \
+--docker-username=<your-name> \ 
+--docker-password=<your-pword> \ 
+--namespace=your-namespace \
+--dry-run=client -o yaml | kubectl apply -f -
 
 ### Configure RBAC ArgoCD
 kubectl edit configmap argocd-rbac-cm -n argocd
