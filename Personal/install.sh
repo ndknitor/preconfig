19996076e30e4358994270ee6270c4d322032db5
KN <64144072+ndknitor@users.noreply.github.com>
2025-02-08 22:50:59 +0700

    Update install.sh

--- Personal/install.sh
+++ Personal/install.sh
@@ -106,3 +106,14 @@ chmod +x $HOME/.local/share/scripts/colorpicker.sh
 
 sudo usermod -a -G dialout $USER
 sudo reboot
+
+#Install nvidia runtime for docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt update
+sudo apt install -y nvidia-container-toolkit
+sudo systemctl restart docker
+
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
