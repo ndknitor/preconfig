d2ca41766e5727fddf47602f76748e2f780de8da
KN <64144072+ndknitor@users.noreply.github.com>
2025-03-09 00:26:45 +0700

    Update command.sh

--- K8S/command.sh
+++ K8S/command.sh
@@ -57,3 +57,39 @@ data:
 
 ### Switch context ###
 kubectl config set-context --current --namespace=asp-template 
+
+### Backup namespaces ###
+kubectl get all --all-namespaces -o yaml > k8s-namespaces.yaml
+
+### Backup ConfigMaps, Secrets, or Custom Resources (CRDs) ###
+kubectl get configmaps --all-namespaces -o yaml > configmaps.yaml
+kubectl get secrets --all-namespaces -o yaml > secrets.yaml
+kubectl get crds -o yaml > crds.yaml
+
+### Backup PV, PVC ###
+kubectl get pv -o yaml > pvs.yaml
+kubectl get pvc --all-namespaces -o yaml > pvcs.yaml
+
+### Backup Cluster Roles and Role Bindings ###
+kubectl get clusterroles -o yaml > clusterroles.yaml
+kubectl get clusterrolebindings -o yaml > clusterrolebindings.yaml
+kubectl get roles --all-namespaces -o yaml > roles.yaml
+kubectl get rolebindings --all-namespaces -o yaml > rolebindings.yaml
+
+
+### Backup ingress service and network policy
+kubectl get ingress --all-namespaces -o yaml > ingress.yaml
+kubectl get services --all-namespaces -o yaml > services.yaml
+kubectl get networkpolicies --all-namespaces -o yaml > networkpolicies.yaml
+
+#Backup everything
+kubectl get $(kubectl api-resources --verbs=list -o name | tr '\n' ',' | sed 's/,$//') --all-namespaces -o yaml > full-k8s-backup.yaml
+
+#Backup PVC data
+export RESTIC_REPOSITORY="s3:http://minio.example.com/k8s-backups"
+export AWS_ACCESS_KEY_ID="your-access-key"
+export AWS_SECRET_ACCESS_KEY="your-secret-key"
+#Init backup
+restic init
+#backup
+restic backup /mnt/pvc-data
